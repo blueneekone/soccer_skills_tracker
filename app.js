36d71a9dc20beaab0cbcfe15620e7145fde55bee
Evan Waechtler <48002879+blueneekone@users.noreply.github.com>
2026-02-22 21:14:52 -0700

    Remove unused import from firebase-config.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// Import from your brand new module!
 import { auth, db } from "./firebase-config.js"; 
-
 import { signInWithRedirect, getRedirectResult, GoogleAuthProvider, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
   from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection, addDoc, query, where, getDocs, orderBy, limit, doc, setDoc, getDoc, updateDoc, writeBatch } 
